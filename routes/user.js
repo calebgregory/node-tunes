308d2d39eeadfc391e49f23b5ea1473bb5f42043
calebgregory <calebgregory@gmail.com>
2015-08-17 15:22:06 -0500

    When a new user registers, redirect them to the main page

--- routes/user.js
+++ routes/user.js
@@ -6,17 +6,40 @@ var User = require(path.join(process.cwd(),
 
 var router = express.Router();
 
-router.post('/', function(req,res) {
+router.post('/new', function(req,res) {
   var u = req.body;
   User.create( u ,
     function(err) {
-      if(err) console.log(err);
-      res.redirect('/');
+      if(err) {
+        console.log(err);
+        res.render('user/new',
+                  { err : err });
+      } else {
+        User.login( u ,
+          function(err,user) {
+            req.session.regenerate(
+              function() {
+                req.session.user = user;
+                res.redirect('/');
+              });
+          });
+      }
+    });
+});
+
+router.get('/new', function(req,res) {
+  req.session.regenerate(
+    function() {
+      res.render('user/new',
+                { err : null });
     });
 });
 
 router.get('/login', function(req,res) {
-  res.render('user/login');
+  req.session.regenerate(
+    function() {
+      res.render('user/login');
+    });
 });
 
 router.post('/login', function(req,res) {
