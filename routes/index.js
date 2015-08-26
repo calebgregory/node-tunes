c3d0afe7664222f2a5ab7c92d67edc56087c416b
calebgregory <calebgregory@gmail.com>
2015-08-26 11:03:17 -0500

    Adding 'home' route

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res) {
-
-  res.redirect('/artists');
-
+  if(req.session.user) res.redirect('/artists');
+  else res.render('home');
 });
 
 module.exports = router;
